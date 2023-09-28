fbddc1a4178329047161e43e243f9e480e3442b9
xeokoin <xeokoin@gmail.com>
2023-09-28 10:44:38 +0300

    Add morning test Day4

--- Day4/main.py
+++ Day4/main.py
@@ -1,4 +1,5 @@
 import excel
 
 
-excel.add_login_password('people_data_dubl.xlsx', 'new_people_data_dubl.xlsx')
+excel.add_login_password('people_data_dubl.xlsx',
+                         'new_people_data_dubl.xlsx')
