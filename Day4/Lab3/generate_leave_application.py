5d84dd1341aa2e615702dd445cbe3f5e5d511862
xeokoin <xeokoin@gmail.com>
2023-09-28 17:18:29 +0300

    Save not valid emails to email_errors.docx

--- Day4/Lab3/generate_leave_application.py
+++ Day4/Lab3/generate_leave_application.py
@@ -1,15 +1,23 @@
 import openpyxl
 from docxtpl import DocxTemplate
 import re
+from docx import Document
 
 
-def generate_leave_application(excel_file: str, excel_sheet: str, doc_template: str):
+def generate_leave_application(excel_file: str, excel_sheet: str, doc_template: str, email_errors_file: str):
     wb = openpyxl.load_workbook(filename=excel_file)
     sheet = wb[excel_sheet]
 
     doc = DocxTemplate(doc_template)
 
+    email_doc = Document()
+    table = email_doc.add_table(rows=1, cols=2, style='Table Grid')
+    table.cell(0, 0).paragraphs[0].add_run('Имя и Фамилия').bold = True
+    table.cell(0, 1).paragraphs[0].add_run('email').bold = True
+
     row_count = sheet.max_row
+
+    wrong_emails = {}
 
     for i in list(range(2, row_count+1)):
         last_name = sheet['A' + str(i)].value
@@ -30,5 +38,16 @@ def generate_leave_application(excel_file: str, excel_sheet: str, doc_template: 
 
         regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
 
+        full_name = str(last_name + " " + name)
+
         if not re.fullmatch(regex, email):
-            print(f'Ошибка в email - {email} для {name} {last_name}')
+            d = {full_name: email}
+            wrong_emails.update(d)
+
+    if len(wrong_emails) > 0:
+        for k, v in wrong_emails.items():
+            row = table.add_row()
+            row.cells[0].text = k
+            row.cells[1].text = v
+
+    email_doc.save(email_errors_file)
--- Day4/Lab3/main.py
+++ Day4/Lab3/main.py
@@ -3,4 +3,5 @@ from generate_leave_application import generate_leave_application
 
 generate_leave_application('people_data_vacation.xlsx',
                            'vacation',
-                           'Заявление на отпуск.docx')
+                           'Заявление на отпуск.docx',
+                           'email_errors.docx')
